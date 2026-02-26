4c7a6e6bb61c98bb0ff6f0025ef32772c03181a6
Tejas bhat <bhattejas616@gmail.com>
2026-02-26 18:13:58 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const jsxElement = <h1>Hello Tejas bhai</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxElement)
